1dd08f71afb21add54a5b06823d574f0db75b4f5
Bárbara Neves <barbara.steph@alu.ufc.br>
2020-08-08 17:28:08 -0300

    [Trabalho 1] Trabalho finalizado

--- Trabalho 1/modules/linear_models.py
+++ Trabalho 1/modules/linear_models.py
@@ -143,10 +143,11 @@ class MLRStochasticGradientDescent():
          
         for e in range(epochs): 
             count = 0.0
-            for i in range(n):
-                y_pred = X_[i].dot(B)
+            random_permutation = np.random.permutation(n)
+            for Xi, yi in zip(X_[random_permutation], y[random_permutation]):
+                y_pred = Xi.dot(B)
 
-                B = B - learning_rate * (X_[i].T.dot(y_pred - y[i]))
+                B = B - learning_rate * (Xi.T.dot(y_pred - yi))
                 count += metrics.MSE(y, y_pred)
             cost[e] = count
             
@@ -202,41 +203,41 @@ class CubicLN():
         return self.MLR.coef_()
 
 # h. Regressão Linear Regularizada Multivariada - Gradiente Descendente
-class LRRegularized():
-    def __init__(self): 
-        pass 
+class MLRRegularized():
+    def __init__(self):
+        pass
 
-    def fit(self, X, y, epochs, learning_rate): 
-        # Inicializando os coeficientes com 0
-        b_0 = 0
-        b_1 = 0
+    def fit(self, X, y, epochs, learning_rate, lamb):
+        n = X.shape[0] # Números de amostras
+        p = X.shape[1] # Número de variáveis (atributos)
+        X_ = np.c_[np.ones(n), X]
         
-        n = len(X)  # Número de observações
         cost = np.zeros(epochs)
-        
-        # Aplicando gradiente descendente
-        for e in range(epochs):
-            y_pred = b_0 + b_1 * X
-      
-            # Calculando derivadas (gradientes)
-            D_0 = (1/n) * sum(y - y_pred)
-            D_1 = (1/n) * sum((y - y_pred) * X) 
-            
-            # Atualizando betas
-            b_0 = b_0 + learning_rate * D_0  
-            b_1 = b_1 + learning_rate * D_1
-            
+        B = np.zeros(p + 1)
+        b_0 = 0
+         
+        for e in range(epochs): 
+            y_pred = X_.dot(B)
+
+            D_0 = (1/n) * sum(y - y_pred) 
+            b_0 = b_0 + learning_rate * D_0
+
+            D = ((1/n) * X_.T.dot(y_pred - y)) - (lamb/n * B)
+            B = B - learning_rate * D
             cost[e] = metrics.MSE(y, y_pred)
-            
-        self.b_0 = b_0
-        self.b_1 = b_1
+        B[0] = b_0
+        
+        self.B = B
         self.cost = cost
-  
-    def predict(self, X):  
-        return self.b_0 + self.b_1 * X
+        
+    def predict(self, X):
+        n = X.shape[0]
+        X_ = np.c_[np.ones(n), X]
+        
+        return X_.dot(self.B)
     
-    def coef_(self):
-        return [self.b_0, self.b_1]
+    def coef_(self):        
+        return self.B
     
-    def cost_history(self): # Apenas para escolher uma época boa
+    def cost_history(self):
         return self.cost
--- Trabalho 1/modules/utils.py
+++ Trabalho 1/modules/utils.py
@@ -8,14 +8,14 @@ from modules import metrics
 
 def plot_line_graphic(X, y, y_pred, X_name, y_name):
     plt.figure(figsize=(10, 5))
-    plt.scatter(X, y, marker='o', s=10)
+    plt.scatter(X, y, marker='o', s=10, color='slategray')
 
     plt.xlabel(X_name)
     plt.ylabel(y_name)
 
-    plt.plot(X, y_pred, color="g");
+    plt.plot(X, y_pred, color='firebrick');
     
-def calculates_error_metrics(clfs, X_train, y_train, X_test, y_test):
+def calculates_error_metrics(models, X_train, y_train, X_test, y_test):
     scores = {}
     scores_tr = {}
     scores_te = {}
@@ -24,7 +24,7 @@ def calculates_error_metrics(clfs, X_train, y_train, X_test, y_test):
     pred_values_tr = {}
     pred_values_te = {}
     
-    for k, v in clfs.items():
+    for k, v in models.items():
         y_pred_train = v.predict(X_train)
         y_pred_test = v.predict(X_test)
         
@@ -46,7 +46,6 @@ def calculates_error_metrics(clfs, X_train, y_train, X_test, y_test):
     return pred_values, scores
 
 def plot_scores_histogram(linear_models, scores, score_name):
-    
     labels = [model + '_' + score_name for model in list(linear_models.keys())]
     train = [float(new_l.quantize(Decimal('.001'), rounding="ROUND_DOWN")) for new_l in [Decimal(scores['Train'][l]) for l in labels]]
     test = [float(new_l.quantize(Decimal('.001'), rounding="ROUND_DOWN")) for new_l in [Decimal(scores['Test'][l]) for l in labels]]
@@ -54,9 +53,9 @@ def plot_scores_histogram(linear_models, scores, score_name):
     x = np.arange(len(labels))  # the label locations
     width = 0.25  # the width of the bars
 
-    fig, ax = plt.subplots(figsize=(15, 8))
-    rects1 = ax.bar(x - width/2, train, width, label='Train')
-    rects2 = ax.bar(x + width/2, test, width, label='Test')
+    fig, ax = plt.subplots(figsize=(12, 8))
+    rects1 = ax.bar(x - width/2, train, width, label='Train', color='slategray')
+    rects2 = ax.bar(x + width/2, test, width, label='Test', color='firebrick')
 
     # Add some text for labels, title and custom x-axis tick labels, etc.
     ax.set_ylabel('Scores', fontsize=14)
@@ -79,4 +78,27 @@ def plot_scores_histogram(linear_models, scores, score_name):
     autolabel(rects1)
     autolabel(rects2)
 
+    plt.show()
+
+def modifying_regularization_coef(X_train, y_train, pred_feature, y_true, model, epochs, learning_rate, max_lamb, metric):
+    values_metric = []
+    
+    for l in range(1, max_lamb + 1):
+        model.fit(X_train, y_train, epochs, learning_rate, lamb=l)
+        y_pred = model.predict(pred_feature)
+        values_metric.append(metric(y_true, y_pred))
+    
+    return values_metric
+
+def plot_lambdas(train_error, test_error):
+    plt.figure(figsize=(10, 5))
+
+    x = range(1, 6) 
+    plt.plot(x, train_error, marker='o', label="Train", color='slategray')
+    plt.plot(x, test_error, marker='o', label = "Test", color='firebrick')
+
+    plt.xlabel('Lambdas')
+    plt.ylabel('MSE')
+
+    plt.legend()
     plt.show()
